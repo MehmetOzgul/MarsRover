bf056f9d6b3434dea991c102f3ffe9cd53eacf05
Mehmet OZGUL <Mehmet.OZGUL@amadeus.com>
2020-04-11 22:17:54 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using HepsiBuradaCase.Entity;
+using System;
+
+namespace HepsiBuradaCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var capacity = Console.ReadLine().Trim().Split(' ');
+            var positions = Console.ReadLine().Trim().Split(' ');
+            var moves = Console.ReadLine();
+
+            var rover = new Rover
+            {
+                X = int.Parse(positions[0]),
+                Y = int.Parse(positions[1]),
+                Direction = Char.Parse(positions[2]),
+                maxX = int.Parse(capacity[0]),
+                maxY = int.Parse(capacity[1])
+            };
+            new Processor(new MoveProcessor()).Run(moves, rover);
+            Console.Write(rover.X + " " + rover.Y + rover.Direction);
+            Console.ReadKey();
+        }
+
+    
+    }
+
+
+   
+
+  
+
+
+
+
+
+
+}
---
+++ Rover.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HepsiBuradaCase.Entity
+{
+    public class Rover
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Direction { get; set; }
+
+        public int maxX { get; set; }
+        public int maxY { get; set; }
+    }
+}
